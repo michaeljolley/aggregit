b2bc3057fe6994637905fbfc8e4dfefd4f1b4597
Michael Jolley <mike@sparcapp.io>
2020-04-13 23:26:26 -0500

    Working on getting repo

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-//import * as core from '@actions/core'
+import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 const run = async (): Promise<void> => {
-  console.dir(github.context.payload)
+  const githubToken: string = process.env.GITHUB_TOKEN || ''
+
+  const octokit: github.GitHub = new github.GitHub(githubToken)
+
+  const {data: repo} = await octokit.repos.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo
+  })
+
+  console.dir(repo)
 }
 
 run()
